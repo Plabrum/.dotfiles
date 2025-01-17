99d298a898c14168384d0c7836f381324af5094b
Phil Labrum <philip.labrum@gmail.com>
2025-01-17 00:17:47 -0500

    improve stow script

--- scripts/stow_files.sh
+++ scripts/stow_files.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
 # Function to stow configurations
 stow_configs() {
-	local -r to_stow="$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
+
+	# Find all directories in /stow
+	to_stow=$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')
 	info "Stowing: $to_stow"
+
+	# Remove existing files managed by stow
+	for dir in $to_stow; do
+		if [[ -d "stow/$dir" ]]; then
+			echo "Removing existing files for $dir"
+			stow -d stow --verbose 1 --target "$HOME" -D "$dir"
+		fi
+	done
+
+	# Stow the configurations
 	stow -d stow --verbose 1 --target "$HOME" "$to_stow"
 }
