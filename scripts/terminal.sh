694f0c0a89f53b8578beafc1d61c668ab0644823
Phil Labrum <philip.labrum@gmail.com>
2025-01-17 00:29:17 -0500

    fix casks syntex

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -61,7 +61,7 @@ install_brew_packages() {
 
 install_brew_casks() {
 	local casks=$*
-	for cask in $casks; do
+	for cask in "${casks[@]}"; do
 		if brew list --casks | grep "$cask" >/dev/null; then
 			warn "Cask $cask is already installed"
 		else
