5cc4b75ba72a0aa41d3330a189ee6b99b5d5551a
Phil Labrum <philip.labrum@gmail.com>
2025-01-16 23:33:32 -0500

    duplicated info and success functions

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ success() {
 	printf "%s[*] %s%s\n" "$(tput setaf 2)" "$1" "$reset_color"
 }
 
-err() {
-	printf "%s[*] %s%s\n" "$(tput setaf 1)" "$1" "$reset_color"
-}
-
-warn() {
-	printf "%s[*] %s%s\n" "$(tput setaf 3)" "$1" "$reset_color"
-}
-
 REPO_URL=https://github.com/plabrum/.dotfiles.git
 REPO_PATH="$HOME/.dotfiles"
 
